3a18bad86b26d82787c612f97da477bdc5e74af3
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-04-30 12:30:47 +0900

    problems commits

---
+++ problems/1.py
@@ -0,0 +1,11 @@
+# row = 5
+# col = 5
+
+# # row 반복
+# for num_row in range(1, row + 1):
+#     # col 반복
+#     for _ in range(num_row):
+#         # "*" 출력
+#         print("* ", end="")
+    
+#     print()
---
+++ problems/login-additional.py
@@ -0,0 +1,11 @@
+# ID 와 PW 설정
+correct_id = "admin"
+correct_pw = "password123"
+# 사용자로 부터 입력
+user_id = input("ID 입력: ")
+user_pw = input("PW 입력: ")
+
+# ID 비교 일치 여부 판별 (반복)
+
+
+# 
