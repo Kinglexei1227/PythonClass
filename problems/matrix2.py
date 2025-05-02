1cb5d798558b2686534b257700658aa7e690f937
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-05-02 12:22:30 +0900

    problems

---
+++ problems/matrix2.py
@@ -0,0 +1,29 @@
+# 2단계
+
+# 랜덤설정
+import random
+# 사용자로부터 입력값 받기
+table = int(input("테이블 개수 입력: "))
+row = int(input("행 수 입력: "))
+col = int(input("열 수 입력: "))
+# 메뉴
+menu = int(input('''
+출력 옵션을 선택하세요:
+1. 1부터 시작하여 열 방향으로 증가
+2. 1~100 사이랜덤 값 출력                 
+옵션 (1 또는 2): '''))
+
+for i in range(1, table+1):
+    if menu == 1:
+        for f in range(row):
+            for f in range(col):
+                print(f + 1, end=" ")
+                
+            print()
+        print()
+    elif menu == 2:
+        for a in range(row):
+            for a in range(col):
+                print(random.randint(1,100), end=" ")
+            print()
+        print()
