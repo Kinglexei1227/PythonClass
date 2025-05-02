1751c218e527bb4a0f669db0f00778dea7fde58e
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-05-02 12:37:02 +0900

    problem 2

--- problems/matrix2.py
+++ problems/matrix2.py
@@ -12,15 +12,17 @@ menu = int(input('''
 1. 1부터 시작하여 열 방향으로 증가
 2. 1~100 사이랜덤 값 출력                 
 옵션 (1 또는 2): '''))
-
+# 반복 설정
+# 1번 옵션
 for i in range(1, table+1):
     if menu == 1:
         for f in range(row):
             for f in range(col):
-                print(f + 1, end=" ")
+                print(f, end=" ")
                 
             print()
         print()
+# 2번 옵션
     elif menu == 2:
         for a in range(row):
             for a in range(col):
