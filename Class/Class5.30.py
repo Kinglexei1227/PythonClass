ddca6314412a253b76b23c6cc43358720064b2ea
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-05-30 14:14:36 +0900

    Class5.30 commit

---
+++ Class/Class5.30.py
@@ -0,0 +1,11 @@
+# 함수 정의
+# -> 자기만의 함수를 만들고 싶을때
+
+
+# bar 함수 정의
+def bar (a, b, c, d, e = 100): # parameter (매개 변수) -> msg
+    print(a, b, c, d, e)
+    
+bar(1, 2, 3, 4, 5) # argument (인자값)
+bar(1, 2, 3, 4)
+bar(1, 2, 3)
