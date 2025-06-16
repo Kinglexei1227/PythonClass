a165a46c6e2ebfef8ec235bfa680a91f3974699c
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-06-16 10:02:16 +0900

    dictionary

---
+++ practice/2.py
@@ -0,0 +1,12 @@
+person_dict = {
+    "name": "Charlie",
+    "age" : 40,
+    "city" : "New York City"
+}
+print("\n2. 수동으로 패킹된 딕셔너리:", person_dict, type(person_dict))
+
+users = [
+    {"id": 1, "name": "철수", "age": 20},
+    {"id": 2, "name": "영희", "age": 22},
+    {"id": 3, "name": "민수", "age": 19}
+]
--- practice/practice1.py
+++ practice/practice1.py
@@ -2,3 +2,5 @@ fruits = ["apple", "banana", "cherry"]
 fruits[1] = "grape"
 fruits.append("mango")
 print(fruits)
+
+crowns = []
